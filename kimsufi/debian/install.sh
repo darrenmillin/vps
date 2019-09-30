3916fee7e89609489cf3c4584b0b3b9d5975d474
Darren Millin <github@git.darrenmillin.com>
2019-09-30 17:28:38 +0100

    Update install.sh

--- kimsufi/debian/install.sh
+++ kimsufi/debian/install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+###########################################
+# Install packages
+###########################################
+
 # Install packages
 apt-get --assume-yes install git sudo tmux bash-completion ca-certificates
 apt-get --assume-yes install rtorrent curl nginx lm-sensors
   
 update-alternatives --set editor /usr/bin/vim.basic
-  
+
+###########################################
+# Create rtorrent user
+###########################################
+
 # Create rtorrent user
 RTORRENT_HOME="/home/rtorrent"
 adduser --home ${RTORRENT_HOME} --disabled-password --shell /bin/bash --gecos "rTorrent User" rtorrent
@@ -18,3 +26,20 @@ mkdir -p ${RTORRENT_HOME}/watch
 
 # Change ownership of ~rtorrent
 chown -R rtorrent:rtorrent ${RTORRENT_HOME}
+
+###########################################
+# Install Resilio
+###########################################
+
+# Add the repo
+echo "deb https://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | tee /etc/apt/sources.list/d/resilio-sync.list
+
+# Get key.asc
+curl -L0 http://linux-packages.resilio.com/resilio-sync/key.asc && apt-key add ./key.asc
+
+# Remove key.asc
+rm ./key.asc
+
+# Install the resilio package
+apt-get update
+apt-get install resilio-sync
