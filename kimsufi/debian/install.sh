dca7abc271e8ccea8bddce080bcaab3fd50a6c74
Darren Millin <github@git.darrenmillin.com>
2019-09-30 16:29:48 +0100

    Update install.sh

--- kimsufi/debian/install.sh
+++ kimsufi/debian/install.sh
@@ -5,11 +5,15 @@ apt-get --assume-yes install rtorrent curl nginx lm-sensors
   
 update-alternatives --set editor /usr/bin/vim.basic
   
+# Create rtorrent user
 RTORRENT_HOME="/home/rtorrent"
 adduser --home ${RTORRENT_HOME} --disabled-password --shell /bin/bash --gecos "rTorrent User" rtorrent
+
+# Create subdirectories
 mkdir -p ${RTORRENT_HOME}/downloads
 mkdir -p ${RTORRENT_HOME}/queue
 mkdir -p ${RTORRENT_HOME}/session
 mkdir -p ${RTORRENT_HOME}/watch
 
+# Change ownership of ~rtorrent
 chown -R rtorrent:rtorrent ${RTORRENT_HOME}
