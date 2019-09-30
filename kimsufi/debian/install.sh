08d004ff922e3d2e8e1ee5c93e11d48b24ecbabd
Darren Millin <github@git.darrenmillin.com>
2019-09-30 16:28:38 +0100

    Update install.sh

--- kimsufi/debian/install.sh
+++ kimsufi/debian/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-  apt-get --assume-yes install git sudo tmux bash-completion ca-certificates
-  apt-get --assume-yes install rtorrent curl nginx lm-sensors
+apt-get --assume-yes install git sudo tmux bash-completion ca-certificates
+apt-get --assume-yes install rtorrent curl nginx lm-sensors
   
-  update-alternatives --set editor /usr/bin/vim.basic
+update-alternatives --set editor /usr/bin/vim.basic
   
-  RTORRENT_HOME="/home/rtorrent"
-  adduser --home ${RTORRENT_HOME} --disabled-password --shell /bin/bash --gecos "rTorrent User" rtorrent
-  mkdir -p ${RTORRENT_HOME}/downloads
-  mkdir -p ${RTORRENT_HOME}/queue
-  mkdir -p ${RTORRENT_HOME}/session
-  mkdir -p ${RTORRENT_HOME}/watch
-  
-  chown -R rtorrent:rtorrent ${RTORRENT_HOME}
+RTORRENT_HOME="/home/rtorrent"
+adduser --home ${RTORRENT_HOME} --disabled-password --shell /bin/bash --gecos "rTorrent User" rtorrent
+mkdir -p ${RTORRENT_HOME}/downloads
+mkdir -p ${RTORRENT_HOME}/queue
+mkdir -p ${RTORRENT_HOME}/session
+mkdir -p ${RTORRENT_HOME}/watch
+
+chown -R rtorrent:rtorrent ${RTORRENT_HOME}
