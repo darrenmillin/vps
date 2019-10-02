4e7611d1e7be060262b72632ec5e89a6ec79d934
Darren Millin <github@git.darrenmillin.com>
2019-10-02 10:31:53 +0100

    Update install.sh

--- kimsufi/debian/install.sh
+++ kimsufi/debian/install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 ###########################################
+# Declare variables
+###########################################
+
+IP_ADDRESS=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+RTORRENT_HOME="/home/rtorrent"
+
+###########################################
+# Configure backports
+###########################################
+
+deb http://deb.debian.org/debian buster-backports main
+
+###########################################
 # Install packages
 ###########################################
 
 # Install packages
 apt-get --assume-yes install git sudo tmux bash-completion ca-certificates
-apt-get --assume-yes install rtorrent curl nginx lm-sensors
-  
+apt-get --assume-yes install inotify-tools unar rtorrent curl nginx lm-sensors
+apt-get --assume-yes install certbot python-certbot-nginx -t stretch-backports
+
 update-alternatives --set editor /usr/bin/vim.basic
 
 ###########################################
@@ -15,7 +29,6 @@ update-alternatives --set editor /usr/bin/vim.basic
 ###########################################
 
 # Create rtorrent user
-RTORRENT_HOME="/home/rtorrent"
 adduser --home ${RTORRENT_HOME} --disabled-password --shell /bin/bash --gecos "rTorrent User" rtorrent
 
 # Create subdirectories
@@ -24,7 +37,95 @@ mkdir -p ${RTORRENT_HOME}/queue
 mkdir -p ${RTORRENT_HOME}/session
 mkdir -p ${RTORRENT_HOME}/watch
 
-# Change ownership of ~rtorrent
+###########################################
+# Configure rTorrent
+###########################################
+
+cat <<-RTORRENT_CONFIG > ${RTORRENT_HOME}/.rtorrent.rc
+#This is an example resource file for rTorrent. Copy to
+# ~/.rtorrent.rc and enable/modify the options as needed. Remember to
+# uncomment the options you wish to enable.
+# Maximum and minimum number of peers to connect to per torrent.
+#min_peers = 40
+#max_peers = 100
+# Same as above but for seeding completed torrents (-1 = same as downloading)
+#min_peers_seed = 10
+#max_peers_seed = 50
+# Maximum number of simultanious uploads per torrent.
+max_uploads = 3
+# Global upload and download rate in KiB. "0" for unlimited.
+download_rate = 2000
+upload_rate = 2000
+# Default directory to save the downloaded torrents.
+directory = ${RTORRENT_HOME}/downloads
+# Default session directory. Make sure you don't run multiple instance
+# of rtorrent using the same session directory. Perhaps using a
+# relative path?
+session = ${RTORRENT_HOME}/session
+# Watch a directory for new torrents, and stop those that have been
+# deleted.
+schedule = watch_directory,5,5,load_start=${RTORRENT_HOME}/watch/*.torrent
+#schedule = untied_directory,5,5,stop_untied=
+# Close torrents when diskspace is low.
+#schedule = low_diskspace,5,60,close_low_diskspace=100M
+# The ip address reported to the tracker.
+ip = ${IP_ADDRESS}
+#ip = rakshasa.no
+# The ip address the listening socket and outgoing connections is
+# bound to.
+bind = ${IP_ADDRESS}
+#bind = rakshasa.no
+# Port range to use for listening.
+port_range = 60125-64125
+# Start opening ports at a random position within the port range.
+port_random = yes
+# Check hash for finished torrents. Might be usefull until the bug is
+# fixed that causes lack of diskspace not to be properly reported.
+check_hash = yes
+# Set whetever the client should try to connect to UDP trackers.
+#use_udp_trackers = yes
+# Alternative calls to bind and ip that should handle dynamic ip's.
+#schedule = ip_tick,0,1800,ip=rakshasa
+#schedule = bind_tick,0,1800,bind=rakshasa
+# Encryption options, set to none (default) or any combination of the following:
+# allow_incoming, try_outgoing, require, require_RC4, enable_retry, prefer_plaintext
+#
+# The example value allows incoming encrypted connections, starts unencrypted
+# outgoing connections but retries with encryption if they fail, preferring
+# plaintext to RC4 encryption after the encrypted handshake
+#
+encryption = allow_incoming,enable_retry,prefer_plaintext
+# Enable DHT support for trackerless torrents or when all trackers are down.
+# May be set to "disable" (completely disable DHT), "off" (do not start DHT),
+# "auto" (start and stop DHT as needed), or "on" (start DHT immediately).
+# The default is "off". For DHT to work, a session directory must be defined.
+#
+# dht = auto
+# UDP port to use for DHT.
+#
+dht_port = 63425
+# Enable peer exchange (for torrents not marked private)
+#
+peer_exchange = yes
+#
+# Do not modify the following parameters unless you know what you're doing.
+#
+# Hash read-ahead controls how many MB to request the kernel to read
+# ahead. If the value is too low the disk may not be fully utilized,
+# while if too high the kernel might not be able to keep the read
+# pages in memory thus end up trashing.
+#hash_read_ahead = 10
+# Interval between attempts to check the hash, in milliseconds.
+#hash_interval = 100
+# Number of attempts to check the hash while using the mincore status,
+# before forcing. Overworked systems might need lower values to get a
+# decent hash checking rate.
+#hash_max_tries = 10
+ratio.enable =
+ratio.min.set=300
+system.method.set = group.seeding.ratio.command, d.close=, d.erase=
+RTORRENT_CONFIG
+
 chown -R rtorrent:rtorrent ${RTORRENT_HOME}
 
 ###########################################
@@ -35,7 +136,7 @@ chown -R rtorrent:rtorrent ${RTORRENT_HOME}
 echo "deb https://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | tee /etc/apt/sources.list/d/resilio-sync.list
 
 # Get key.asc
-curl -L0 http://linux-packages.resilio.com/resilio-sync/key.asc && apt-key add ./key.asc
+curl -L0 https://linux-packages.resilio.com/resilio-sync/key.asc && apt-key add ./key.asc
 
 # Remove key.asc
 rm ./key.asc
@@ -47,4 +148,8 @@ apt-get install resilio-sync
 # Enable the resilio-sync service
 systemctl enable resilio-sync
 
+###########################################
 # Update the nginx config
+###########################################
+
+
