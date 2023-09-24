22ed320e1c6e07089f87d9d9b6ea8c30fa7ea475
Darren Millin <github@git.darrenmillin.com>
2023-09-24 11:58:42 +0100

    Update install.sh

--- kimsufi/debian/docker/install.sh
+++ kimsufi/debian/docker/install.sh
@@ -23,17 +23,17 @@ update-alternatives --set editor /usr/bin/vim.basic
 ###########################################
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
+apt-get update
+apt-get  --assume-yes install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 ###########################################
