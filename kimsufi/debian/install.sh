b9085f8565203c75963b54f418cee1a9cb38c2c3
Darren Millin <github@git.darrenmillin.com>
2023-09-27 22:17:07 +0100

    Update install.sh

--- kimsufi/debian/install.sh
+++ kimsufi/debian/install.sh
@@ -16,7 +16,7 @@ NGINX_HOME="/data/${NGINX_USER}"
 RESILIO_SYNC_USER="sync"
 RESILIO_SYNC_HOME="/data/${RESILIO_SYNC_USER}"
 RTORRENT_USER="rtorrent"
-RTORRENT_HOME="/home/${RTORRENT_USER}"
+RTORRENT_HOME="/data/${RTORRENT_USER}"
 RTORRENT_DATA_HOME="/data/${RTORRENT_USER}"
 
 ##############################################
@@ -68,7 +68,94 @@ sudo mkdir ${DOCKER_HOME}/scripts
 # Create Docker scripts
 ##############################################
 
-cat <<-COMPOSE >> ${DOCKER_HOME}/scripts/compose.yaml
+cat <<-ENV >> ${RTORRENT_HOME}/compose/.env
+RT_DHT_PORT=6881
+XMLRPC_PORT=8000
+RUTORRENT_PORT=8080
+WEBDAV_PORT=9000
+RT_INC_PORT=50000
+ENV
+
+cat <<-COMPOSE >> ${RTORRENT_HOME}/compose/compose.yaml
+name: rtorrent-rutorrent
+
+services:
+  geoip-updater:
+    image: crazymax/geoip-updater:latest
+    container_name: geoip-updater
+    networks:
+      - rtorrent-rutorrent
+    volumes:
+      - "./data/geoip:/data"
+    env_file:
+      - "./geoip-updater.env"
+    restart: always
+
+  rtorrent-rutorrent:
+    image: darrenmillin/rtorrent-rutorrent:latest
+    container_name: rtorrent-rutorrent
+    networks:
+      - rtorrent-rutorrent
+    expose:
+      - "${RT_DHT_PORT}/udp"
+      - "${XMLRPC_PORT}"
+      - "${RUTORRENT_PORT}"
+      - "${WEBDAV_PORT}"
+      - "${RT_INC_PORT}"
+    ports:
+      - target: ${RT_DHT_PORT}
+        published: ${RT_DHT_PORT}
+        protocol: udp
+      - target: ${RUTORRENT_PORT}
+        published: ${RUTORRENT_PORT}
+        protocol: tcp
+      - target: ${WEBDAV_PORT}
+        published: ${WEBDAV_PORT}
+        protocol: tcp
+      - target: ${RT_INC_PORT}
+        published: ${RT_INC_PORT}
+        protocol: tcp
+    env_file:
+      - "rtorrent-rutorrent.env"
+      - ".env"
+    volumes:
+      - "./data:/data"
+      - "./downloads:/downloads"
+      - "./passwd:/passwd"
+ #   ulimits:
+ #     nproc: 65535
+ #     nofile:
+ #       soft: 32000
+ #       hard: 40000
+    restart: always
+
+  rtorrent-logs:
+    image: bash
+    container_name: rtorrent-rutorrent-logs
+    command: bash -c 'tail -f /log/*.log'
+    network_mode: none
+    depends_on:
+      - rtorrent-rutorrent
+    volumes:
+      - "./data/rtorrent/log:/log"
+    restart: always
+
+networks:
+  rtorrent-rutorrent:
+    name: rtorrent-rutorrent
+COMPOSE
+
+cat <<-GEOIP_UPDATER_ENV >> ${RTORRENT_HOME}/compose/geoip-updater.env
+TZ=Europe/Paris
+EDITION_IDS=GeoLite2-City,GeoLite2-Country
+LICENSE_KEY=
+DOWNLOAD_PATH=/data
+SCHEDULE=0 0 * * 0
+LOG_LEVEL=info
+LOG_JSON=false
+GEOIP_UPDATER_ENV
+
+cat <<-COMPOSE >> ${RTORRENT_HOME}/compose/compose.yaml
 name: rtorrent-rutorrent
 version: "3"
 services:
@@ -90,6 +177,49 @@ services:
       - 9000:9000
     restart: always
 COMPOSE
+
+cat <<-RTORRENT_RUTORRENT_ENV >> ${RTORRENT_HOME}/compose/rtorrent-rutorrent.env
+TZ=Europe/Paris
+PUID=1000
+PGID=1000
+
+WAN_IP_CMD=false
+
+AUTH_DELAY=0s
+MEMORY_LIMIT=256M
+UPLOAD_MAX_SIZE=16M
+OPCACHE_MEM_SIZE=128
+MAX_FILE_UPLOADS=50
+REAL_IP_FROM=0.0.0.0/32
+REAL_IP_HEADER=X-Forwarded-For
+LOG_IP_VAR=remote_addr
+LOG_ACCESS=true
+
+XMLRPC_AUTHBASIC_STRING=rTorrent XMLRPC restricted access
+RUTORRENT_AUTHBASIC_STRING=ruTorrent restricted access
+WEBDAV_AUTHBASIC_STRING=WebDAV restricted access
+
+RT_LOG_LEVEL=info
+RT_LOG_EXECUTE=false
+RT_LOG_XMLRPC=false
+
+RU_REMOVE_CORE_PLUGINS=httprpc
+RU_HTTP_USER_AGENT=Mozilla/5.0 (Windows NT 6.0; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0
+RU_HTTP_TIME_OUT=30
+RU_HTTP_USE_GZIP=true
+RU_RPC_TIME_OUT=5
+RU_LOG_RPC_CALLS=false
+RU_LOG_RPC_FAULTS=true
+RU_PHP_USE_GZIP=false
+RU_PHP_GZIP_LEVEL=2
+RU_SCHEDULE_RAND=10
+RU_LOG_FILE=/data/rutorrent/rutorrent.log
+RU_DO_DIAGNOSTIC=true
+RU_SAVE_UPLOADED_TORRENTS=true
+RU_OVERWRITE_UPLOADED_TORRENTS=false
+RU_FORBID_USER_SETTINGS=false
+RU_LOCALE=UTF8
+RTORRENT_RUTORRENT_ENV
 
 cat <<-SCRIPT >> ${DOCKER_HOME}/scripts/start_rtorrent_rutorrent.sh
 docker run -d --name rtorrent_rutorrent \
