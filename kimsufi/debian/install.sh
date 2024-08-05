0834567c951f59e9ebc0e287018ad9668858107b
Darren Millin <github@git.darrenmillin.com>
2024-08-05 22:24:02 +0100

    Update install.sh

--- kimsufi/debian/install.sh
+++ kimsufi/debian/install.sh
@@ -1952,10 +1952,6 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-# Source fzf
-source <(fzf --zsh)
-
 ZSHRC
 }
 
